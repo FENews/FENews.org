8deb39cc141af6b1c2436fe478359bb274d07aea
leyayun <auneeyy@gmail.com>
2019-02-03 01:01:10 +0800

    reset contact email

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
     photo: '/photo.jpg',
     bio: 'FENews 是一个前端技术分享的组织，我们会定期原创/翻译前端相关技术文章。',
     contacts: {
-      email: 'auneeyy@gmail.com',
+      email: 'lifeyun@outlook.com',
       github: 'fenews',
       rss: '#',
     }
