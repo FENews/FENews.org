ea1cd41bf9edaedfe61a917d51b537ea46aa1c68
xueqingxiao <xueqingxiao@gmail.com>
2019-03-11 18:01:40 +0800

    chore: formmat

--- src/components/frontmatter.js
+++ src/components/frontmatter.js
@@ -1,30 +1,53 @@
-import React from "react"
+import React from "react";
 
-const Frontmatter = ({frontmatter}) => {
+const Frontmatter = ({ frontmatter }) => {
   debugger;
-  return <>
-    {frontmatter.date}
-    &nbsp;&nbsp;
-    {frontmatter.authors && frontmatter.authors.length > 0 ? '作者：' : null}
-    {frontmatter.authors && frontmatter.authors.map((author, index) => (
-      <span key={author.id} >
-        {
-          !author.social
-            ? <span>{author.name}</span>
-            : <a target="_blank" rel="noopener noreferrer" href={author.social}>{author.name}</a>
-        }
-        {index < frontmatter.authors.length - 1 ? <span>,&nbsp;</span> : <span>&nbsp;</span>}
-      </span>
-    ))}
-    &nbsp;&nbsp;
-    {frontmatter.translators && frontmatter.translators.length > 0 ? '译者：' : null}
-    {frontmatter.translators && frontmatter.translators.map((translator, index) => (
-      <span key={translator.id}>
-        <a target="_blank" rel="noopener noreferrer" key={translator.id} href={translator.social}>{translator.name}</a>
-        {index < frontmatter.translators.length - 1 ? <span>,&nbsp;</span> : <span>&nbsp;</span>}
-      </span>
-    ))}
-  </>
-}
+  return (
+    <>
+      {frontmatter.date}
+      &nbsp;&nbsp;
+      {frontmatter.authors && frontmatter.authors.length > 0 ? "作者：" : null}
+      {frontmatter.authors &&
+        frontmatter.authors.map((author, index) => (
+          <span key={author.id}>
+            {!author.social ? (
+              <span>{author.name}</span>
+            ) : (
+              <a target="_blank" rel="noopener noreferrer" href={author.social}>
+                {author.name}
+              </a>
+            )}
+            {index < frontmatter.authors.length - 1 ? (
+              <span>,&nbsp;</span>
+            ) : (
+              <span>&nbsp;</span>
+            )}
+          </span>
+        ))}
+      &nbsp;&nbsp;
+      {frontmatter.translators && frontmatter.translators.length > 0
+        ? "译者："
+        : null}
+      {frontmatter.translators &&
+        frontmatter.translators.map((translator, index) => (
+          <span key={translator.id}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              key={translator.id}
+              href={translator.social}
+            >
+              {translator.name}
+            </a>
+            {index < frontmatter.translators.length - 1 ? (
+              <span>,&nbsp;</span>
+            ) : (
+              <span>&nbsp;</span>
+            )}
+          </span>
+        ))}
+    </>
+  );
+};
 
 export default Frontmatter;
